95089c4fc938fe7858b3caca997c7a769039c126
JulianArbini97 <1890@holbertonschool.com>
2021-03-10 20:32:34 +0000

    fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,7 +13,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0)
+	if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0 && binary_tree_height(tree->left) && binary_tree_height(tree->right))
 		return (1);
 	return (0);
 }
